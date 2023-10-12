ed96f77874801fb9d11b2a9a5dca16f1455507f0
Lurius-Kitsune <LucasFox@outlook.fr>
2023-10-12 11:54:40 +0200

    [Edit] Modification du script BDD e cas de restauration [New] AJout d'un script pour les mmise à jours de version (script à executer à chaque maj) [New] COnexion fait par le hash (SECURISATION)

--- resources/bdd/gsb_restore.sql
+++ resources/bdd/gsb_restore.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS visiteur (
   nom char(30) DEFAULT NULL,
   prenom char(30)  DEFAULT NULL, 
   login char(20) DEFAULT NULL,
-  mdp char(20) DEFAULT NULL,
+  mdp CHAR(128) DEFAULT NULL,
   adresse char(30) DEFAULT NULL,
   cp char(5) DEFAULT NULL,
   ville char(30) DEFAULT NULL,
