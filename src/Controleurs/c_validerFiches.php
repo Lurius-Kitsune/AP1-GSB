1e8516861cd695575860a3a80f959a5df67f9ca6
Lurius-Kitsune <LucasFox@outlook.fr>
2023-11-23 10:44:04 +0100

    Action refusé avec affichage du bouton
    Fixes #9
    Task #9 - [US] :  Refuser hors forfait

--- src/Controleurs/c_validerFiches.php
+++ src/Controleurs/c_validerFiches.php
@@ -20,18 +20,30 @@
  */
 use Outils\Utilitaires;
 
+$selectedVisiteurId = null;
+
 $lesMois = $pdo->getMoisFichesFraisCloturer();
 
 
-$selectedVisiteurId = null;
+
 $selectedMonth = filter_input(INPUT_GET, 'month', FILTER_SANITIZE_FULL_SPECIAL_CHARS) ?? $lesMois[0]["numAnnee"] . $lesMois[0]["numMois"];
+
+if (!empty($_POST)){
+    if($_POST['buttonInput'] == 'refuser'){
+        $selectedVisiteurId = filter_input(INPUT_GET, 'visiteurId', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $idLigneHorsForfait = filter_input(INPUT_POST, 'idLigneHorsForfait', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $pdo->denyUnFraisHorsForfait($idLigneHorsForfait, $selectedVisiteurId);
+    }
+}
 
 if (isset($_GET['visiteurId']) && $_GET['visiteurId'] != 'none') {
     $selectedVisiteurId = filter_input(INPUT_GET, 'visiteurId', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $infoFraisForfait = $pdo->getLesFraisForfait($selectedVisiteurId, $selectedMonth);
     $listeFraisHorsForfait = $pdo->getLesFraisHorsForfait($selectedVisiteurId, $selectedMonth);
-}
+} 
 
 $visiteurs = $pdo->getVisiteurHavingFicheMonth($selectedMonth);
+
+
 
 require PATH_VIEWS . 'validerFiches/v_validerFiches.php';
--- src/Vues/validerFiches/v_tableFichesHorsForfait.php
+++ src/Vues/validerFiches/v_tableFichesHorsForfait.php
@@ -15,28 +15,33 @@
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  */
 ?>
-<form action="">
+<form action="/?uc=validerFiches&month=<?= $selectedMonth; ?>&visiteurId=<?= $selectedVisiteurId; ?>" method="post">
+    <input id="inputIdLigne" name="idLigneHorsForfait" type="hidden" value="<?= $fraisHorsForfait['id']; ?>"/>
     <tr>
         <td>
             <div class="form-group">
-                <input type="date" class="form-control" id="inputDate" placeholder="01/01/0001" value="<?= $fraisHorsForfait['4'] ?>">
+                <input type="date" class="form-control" name="dateLigneHorsForfait" id="inputDate" placeholder="01/01/0001" value="<?= $fraisHorsForfait['4'] ?>">
             </div>
         </td>
         <td>
             <div class="form-group">
-                <input type="text" class="form-control" id="inputLibelle" placeholder="Libellé" value="<?= $fraisHorsForfait['libelle'] ?>">
+                <div class="input-group" <?= !$fraisHorsForfait['isDeny'] ? 'style="width: 100%;"' : ''; ?>>
+                    <?= $fraisHorsForfait['isDeny'] ? '<div class="input-group-addon" style="background-color: #c9302c; color: white;">Refusé</div>' : ''; ?>
+                    <input type="text" class="form-control" name="libelleLigneHorsForfait" id="inputLibelle" placeholder="Libellé" value="<?= $fraisHorsForfait['libelle'] ?>">
+                </div>
             </div>
         </td>
         <td>
             <div class="form-group">
                 <div class="input-group">
-                    <input type="number" class="form-control" id="inputPrix" placeholder="0.00" value="<?= $fraisHorsForfait['montant'] ?>">
+                    <input type="number" class="form-control" name="prixLigneHorsForfait" id="inputPrix" placeholder="0.00" value="<?= $fraisHorsForfait['montant'] ?>">
                     <div class="input-group-addon">€</div>
                 </div>
             </div>
         </td>
         <td>
-            <button type="submit" class="btn btn-success">Corriger</button>
+            <button type="submit" class="btn btn-success" name="buttonInput" value="corriger">Corriger</button>
+            <button type="submit" class="btn btn-danger" name="buttonInput" <?= (bool)$fraisHorsForfait['isDeny'] ?? false ? 'disabled' : 'value="refuser"' ?>>Refuser</button>
             <button type="reset" class="btn btn-danger">Réinitialiser</button>
         </td>
     </tr>
