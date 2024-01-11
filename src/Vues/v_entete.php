4e1caeab3f2d25abac54118be7b47a3237cf6210
Lurius-Kitsune <LucasFox@outlook.fr>
2024-01-11 09:46:26 +0100

    Merge origin/us/45 into us/45

--- src/Vues/v_entete.php
+++ src/Vues/v_entete.php
@@ -35,7 +35,6 @@
         <!-- Bootstrap Select JS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
         <link href="./styles/style.css" rel="stylesheet">
-        <script src="js/affichageNbLignes.js" defer></script>
     </head>
     <body>
         <style>
